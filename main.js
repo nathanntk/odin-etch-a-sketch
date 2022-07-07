235e871f121904c02d51e5caef8fa2ce1c30b700
NathanK <nathantkevil@gmail.com>
2022-07-07 15:17:28 -0500

    rework templates

---
+++ main.js
@@ -0,0 +1,17 @@
+
+const grid = document.querySelector('grid');
+
+const divs = [16][16];
+
+/*function makeRows(rows, cols) {
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-cols', cols);
+
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        cell.innerText = (c+1);
+        grid.appendChild(cell).className = 'grid-item';
+    };
+};
+
+makeRows(16, 16);*/
